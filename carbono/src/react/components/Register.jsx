dd5a56a0f656b8191ef824a20ec98b00c6742322
Juan Cruz Rey <juancurey2@gmail.com>
2022-11-21 16:42:39 -0300

    set puerto 80

--- carbono/src/react/components/Register.jsx
+++ carbono/src/react/components/Register.jsx
@@ -11,11 +11,11 @@ export const Register = () => {
     
     // es mi estado local que lo llamo usuario , lo arranco VACIO sin propiedades, el setRegister se encarga 
     //de setiarme mi usuario
-
+    //https://carbonoapplication.herokuapp.com/register
 
     function onSubmit(e) {
         e.preventDefault();
-        axios.post("https://carbonoapplication.herokuapp.com/register", JSON.stringify(usuario)).then((data) => { //mando este usuario creado a ese esa url mediando un post obviamente (a mi back), lo mando en tipo json, (por eso json.usuario), (acordarce que este usuario es lo que comenzo como un estado local vacio y se fue haciendo en los inputs y las funciones), y acordarce juan que con el .then estoy haciendo una promesa, es decir hay algo que me va a devolver mi back luego de que yo le mande el usuario y lo tengo que atajar. Si sale todo bien me cae en el then.Esto quiere decir que va a estar en mi data lo que me haya mandado mi back. Acordarce que data es una palabra reservada que puse yo y que va a lamacenar cualquier cosa que yo le mand edel back.
+        axios.post("localhost:80/register", JSON.stringify(usuario)).then((data) => { //mando este usuario creado a ese esa url mediando un post obviamente (a mi back), lo mando en tipo json, (por eso json.usuario), (acordarce que este usuario es lo que comenzo como un estado local vacio y se fue haciendo en los inputs y las funciones), y acordarce juan que con el .then estoy haciendo una promesa, es decir hay algo que me va a devolver mi back luego de que yo le mande el usuario y lo tengo que atajar. Si sale todo bien me cae en el then.Esto quiere decir que va a estar en mi data lo que me haya mandado mi back. Acordarce que data es una palabra reservada que puse yo y que va a lamacenar cualquier cosa que yo le mand edel back.
             //. Si hay problema me va al catch.ya sea problemas de comunicacion de servidor del cliente o nuestro. O tambien puede pasar que no cumpla logica necesaria como que la contraseña no sea correcta, enotnces esto se mando el usuario en json JSON.stringify(usuario)) , ahi se ejecuta un wait() hasta que el back procesa y manda un signal() para que se termine de ejecutar la promesa en el then(), si todo bien too ok, sino cumplio logica como deciamos mi back catghea ese error y le dispara el error a este servidor.
             console.log("funciono", data)
             navigate("/login")
